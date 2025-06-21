e9f702154e71ef582a7305e24f07ab98ee1afe29
Jefferson <Jefferson@JEFFFERSON>
2025-06-20 19:33:47 -0500

    campo para ingresar monto

--- src/components/CurrencyConverter.vue
+++ src/components/CurrencyConverter.vue
@@ -2,13 +2,27 @@
   <div class="q-pa-md">
     <q-card flat bordered class="q-pa-lg">
       <div class="text-h5 text-center q-mb-md">💱 Conversión de Monedas</div>
+
+      <!-- Campo para ingresar monto -->
+      <q-input
+        v-model.number="amount"
+        label="Monto a convertir"
+        type="number"
+        outlined
+        class="q-mb-md"
+      />
     </q-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CurrencyConverter'
+  name: 'CurrencyConverter',
+  data() {
+    return {
+      amount: null
+    }
+  }
 }
 </script>
 
